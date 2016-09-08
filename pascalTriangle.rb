487d74e5f4923a963cdd0a9a3ac94e8cf4b4acc1
arafin9 <arafinsaad@gmail.com>
2016-09-08 16:11:20 +0600

    Update pascalTriangle.rb

--- pascalTriangle.rb
+++ pascalTriangle.rb
@@ -4,17 +4,16 @@ class PascalTriangle
 
 	def initialize(n)
 		@n = n
-
 	end
 def triangle
     (0..n).each{|r|
-            list=[1]
-            numbers=1
-            k=1
+            list = [1]
+            numbers = 1
+            k = 1
             (0..r-1).step(1){
-                    numbers=numbers*(r-k+1)/k
+                    numbers = numbers * (r-k+1)/k
                     list.push numbers
-                    k+=1
+                    k += 1
             }
             p list
     }
